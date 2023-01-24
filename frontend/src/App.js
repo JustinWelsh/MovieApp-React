4a84784ccdc7c83ce611175c28c4b3ef338940db
Justin Welsh <justinwelsh33@gmail.com>
2023-01-24 01:30:53 -0600

    tailwindcss/daisyUI install

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,27 @@
-import './App.css';
-
+import "./App.css";
 
 function App() {
-  console.log(process.env.REACT_APP_SECRET_KEY) // test only, need to delete
+  console.log(process.env.REACT_APP_SECRET_KEY); // test only, need to delete
   return (
     <div className="App">
       <header className="App-header">
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <div className="card w-96 bg-base-100 shadow-xl">
+          <figure>
+            <img src="https://placeimg.com/400/225/arch" alt="Shoes" />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">Shoes!</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Buy Now</button>
+            </div>
+          </div>
+        </div>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>
-          Hello World
-        </p>
+        <p>Hello World</p>
         <a
           className="App-link"
           href="https://reactjs.org"
