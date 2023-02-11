fae32bf2f7b35dd3c95a15f0e3b6bd8111ce484d
Justin Welsh <justinwelsh33@gmail.com>
2023-02-11 05:49:52 -0600

    clean up

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -6,9 +6,6 @@ const Header = () => {
         <div className="hero my-hero">
             <div className="my-hero-content">
                 <h1 className="mb-5 text-6xl ">Find your film</h1>
-                <a href="#"className="mb-5">
-                My watchlist
-                </a>
             </div>
         </div>
     </div>
