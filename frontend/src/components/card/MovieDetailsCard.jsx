93bf6662055c94fbbc3ee644b1ea6e843025d82d
Justin Welsh <justinwelsh33@gmail.com>
2023-02-13 18:49:05 -0600

    separated DetailsCard into its own component

---
+++ frontend/src/components/card/MovieDetailsCard.jsx
@@ -0,0 +1,56 @@
+import { SiRottentomatoes } from 'react-icons/si';
+import { PlusMinus } from '../swap/PlusMinus';
+export function MovieDetailsCard(props) {
+  return (
+    <>
+      <figure className="bg-black shadow-xl">
+        <img
+          src={props.movieDetailsData?.Poster}
+          alt={props.movieDetailsData?.Title}
+        />
+      </figure>
+      <div className="card-body pb-0">
+        <h2 className="card-title text-black">
+          {props.movieDetailsData?.Title}
+        </h2>
+        <div className="flex">
+          <p>{props.movieDetailsData?.Rated}</p>
+          <div className="flex items-center">
+            <div className="text-red-600 text-xl">
+              <SiRottentomatoes />
+            </div>
+            <p className="pl-2">{props.movieDetailsData?.Ratings[1].Value}</p>
+          </div>
+        </div>
+        <div className="flex text-black text-sm my-2">
+          <p>{props.movieDetailsData?.Runtime}</p>
+          <p>{props.movieDetailsData?.Genre}</p>
+          <div className="flex items-center">
+            <PlusMinus>
+              <p className="relative left-3">Watchlist</p>
+            </PlusMinus>
+          </div>
+        </div>
+        <p className="pb-7">{props.movieDetailsData?.Plot}</p>
+        <div className="text-sm pb-3">
+          <p>
+            <span className="text-black">Director:</span>{" "}
+            {props.movieDetailsData?.Director}
+          </p>
+          <p>
+            <span className="text-black">Starring:</span>{" "}
+            {props.movieDetailsData?.Actors}
+          </p>
+        </div>
+        <div className="card-actions justify-end">
+          <button
+            className="btn btn-primary"
+            onClick={() => props.setIsModalOpen(false)}
+          >
+            Close
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
--- frontend/src/components/modal/MovieDetailsModal.jsx
+++ frontend/src/components/modal/MovieDetailsModal.jsx
@@ -1,53 +1,13 @@
 import { SiRottentomatoes } from 'react-icons/si';
 import { PlusMinus } from '../swap/PlusMinus';
+import { MovieDetailsCard } from '../card/MovieDetailsCard';
 
 const MovieDetailsModal = (props) => {
     let modalToggle = props.isModalOpen ? "show" : null //place in container div for animation | may need to delete
   return (
     <div className={`modal-container`}>
       <div className="my-modal">
-        <figure className="bg-black shadow-xl">
-          <img
-            src={props.movieDetailsData?.Poster}
-            alt={props.movieDetailsData?.Title}
-          />
-        </figure>
-        <div className="card-body pb-0">
-          <h2 className="card-title text-black">
-            {props.movieDetailsData?.Title}
-          </h2>
-          <div className="flex">
-            <p>{props.movieDetailsData?.Rated}</p>
-            <div className='flex items-center'>
-                <div className='text-red-600 text-xl'><SiRottentomatoes /></div>
-                <p className='pl-2'>{props.movieDetailsData?.Ratings[1].Value}</p>
-            </div>
-          </div>
-          <div className="flex text-black text-sm my-2">
-            <p>{props.movieDetailsData?.Runtime}</p>
-            <p>{props.movieDetailsData?.Genre}</p>
-            <div className='flex items-center'>
-              <PlusMinus >
-                <p className="relative left-3">Watchlist</p>
-              </PlusMinus >
-            </div>
-          </div>
-          <p className="pb-7">{props.movieDetailsData?.Plot}</p>
-          <div className="text-sm pb-3">
-            <p>
-                <span className='text-black'>Director:</span> {props.movieDetailsData?.Director}</p>
-            <p>
-                <span className='text-black'>Starring:</span> {props.movieDetailsData?.Actors}</p>
-          </div>
-          <div className="card-actions justify-end">
-            <button
-              className="btn btn-primary"
-              onClick={() => props.setIsModalOpen(false)}
-            >
-              Close
-            </button>
-          </div>
-        </div>
+        <MovieDetailsCard {...props}/>
       </div>
     </div>
   );
