179b1271b728efc8cc445306074084e076c6696d
Justin Welsh <justinwelsh33@gmail.com>
2023-03-01 00:01:08 -0600

    WIP

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -5,7 +5,8 @@ const SearchBar = (props) => {
   const [movieTitle, setMovieTitle] = useState(""); //input from the user
   const baseUrl = `http://www.omdbapi.com/?apikey=${process.env.REACT_APP_OMDB_KEY}`;
 
-  const handleSearchClick = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault()
     if (movieTitle) {
       fetch(`${baseUrl}&type=movie&s=${movieTitle}&page=1`)
         .then((res) => res.json())
@@ -14,7 +15,7 @@ const SearchBar = (props) => {
           props.setApiData(data);
         });
       console.log(movieTitle);
-      // setMovieTitle('')
+      setMovieTitle('')
     }
   };
   const handleInputChange = (e) => {
@@ -24,24 +25,27 @@ const SearchBar = (props) => {
   return (
     <div className="search-bar">
       <div className="search-bar w-1/2 mt-10 bg-red-200">
-        <label htmlFor="movie-input" className="relative top-4 left-7">
-          <AiOutlineSearch />
-        </label>
-        <input
-          type="search"
-          id="movie-input"
-          placeholder="Search for a movie"
-          className="input input-bordered w-2/3 bg-white rounded-none rounded-l-lg px-9"
-          name="movie-input"
-          onChange={handleInputChange}
-          value={movieTitle}
-        ></input>
-        <button
-          onClick={handleSearchClick}
-          className="search-btn btn btn-primary w-1/3 rounded-none rounded-r-lg"
-        >
-          Search
-        </button>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="movie-input" className="relative top-8 left-3">
+            <AiOutlineSearch />
+          </label>
+          <input
+            type="search"
+            id="movie-input"
+            placeholder="Search for a movie"
+            className="input input-bordered w-2/3 bg-white rounded-none rounded-l-lg px-9"
+            name="movie-input"
+            onChange={handleInputChange}
+          ></input>
+          <button
+            type="submit"
+            disabled={movieTitle ? false : true}
+            className="search-btn btn-primary w-1/3 rounded-none rounded-r-lg"
+          >
+            Search
+          </button>
+
+        </form>
       </div>
     </div>
   );
