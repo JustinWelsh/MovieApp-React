9aaaa649d27301b805de21edfebc81499b6d00ae
Justin Welsh <justinwelsh33@gmail.com>
2024-03-16 17:50:36 -0500

    small UI update

--- src/routes/Search.jsx
+++ src/routes/Search.jsx
@@ -5,16 +5,18 @@ import MovieCard from "../components/ui/card/MovieCard";
 const Search = () => {
   const { searchedMovies } = useMovieSearchContext();
   return (
-    <div>
-      Search Works!!
-      {searchedMovies.results && (
-        <div className="flex flex-wrap gap-3">
-          {searchedMovies.results.map((movie, index) => {
-            return <MovieCard movie={movie} key={index} />;
-          })}
-        </div>
-      )}
-    </div>
+    <section>
+      <div className="p-8">
+        Search Works!!
+        {searchedMovies.results && (
+          <div className="flex flex-wrap gap-3">
+            {searchedMovies.results.map((movie, index) => {
+              return <MovieCard movie={movie} key={index} />;
+            })}
+          </div>
+        )}
+      </div>
+    </section>
   );
 };
 
