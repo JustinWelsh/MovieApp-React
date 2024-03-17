db6bb9e6b040252942f3558a353e43ef9ce6054c
Justin Welsh <justinwelsh33@gmail.com>
2024-03-16 21:02:39 -0500

    Searched movies modal works

--- src/routes/Search.jsx
+++ src/routes/Search.jsx
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { useMovieSearchContext } from "../context/MovieSearchContext";
 import MovieCard from "../components/ui/card/MovieCard";
+import MovieModal from "../components/ui/MovieModal";
+import { useDisclosure } from "@nextui-org/react";
 
 const Search = () => {
   const { searchedMovies } = useMovieSearchContext();
+  const [selectedMovie, setSelectedMovie] = useState({});
+
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+
   const handleClick = (movie) => {
+    setSelectedMovie(movie);
+    onOpen();
     console.log("Clicked", movie);
   };
   return (
     <section>
+      <MovieModal
+        isOpen={isOpen}
+        onOpenChange={onOpenChange}
+        selectedMovie={selectedMovie}
+      />
       <div className="p-8">
         Search Works!!
         {searchedMovies.results && (
