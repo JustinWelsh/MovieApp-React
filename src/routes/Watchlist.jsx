c2b75cb738ab06ad822ad343e052f3bb718f425c
Justin Welsh <justinwelsh33@gmail.com>
2024-03-17 12:35:21 -0500

    Add watchlist movies modal.

--- src/routes/Watchlist.jsx
+++ src/routes/Watchlist.jsx
@@ -1,19 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import { useWatchlistContext } from "../context/WatchlistContext";
 import MovieCard from "../components/ui/card/MovieCard";
+import MovieModal from "../components/ui/MovieModal";
+import { useDisclosure } from "@nextui-org/react";
 
 const Watchlist = () => {
   const { watchlist } = useWatchlistContext();
   console.log("Watchlist Page", watchlist);
+  const [selectedMovie, setSelectedMovie] = useState({});
+
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+
+  const handleClick = (movie) => {
+    setSelectedMovie(movie);
+    onOpen();
+    console.log("Clicked", movie);
+  };
   return (
     <div className="min-h-[1011px]">
       Watchlist Page!!
+      <MovieModal
+        isOpen={isOpen}
+        onOpenChange={onOpenChange}
+        selectedMovie={selectedMovie}
+      />
       <div className="p-8">
         {watchlist && (
           <div className="flex flex-wrap gap-3">
             {watchlist.map((movie, index) => {
               return (
-                <div key={index}>
+                <div key={index} onClick={() => handleClick(movie)}>
                   <MovieCard movie={movie} />
                 </div>
               );
