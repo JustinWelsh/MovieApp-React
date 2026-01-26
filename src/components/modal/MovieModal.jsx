10f373c989aeca40b15d6df9ff084e6e8940eebd
Justin Welsh <justinwelsh33@gmail.com>
2026-01-26 01:02:10 -0600

    refactor for iframe

--- src/components/modal/MovieModal.jsx
+++ src/components/modal/MovieModal.jsx
@@ -12,7 +12,10 @@ import { fetchTrailer } from "../../services/MovieService";
 
 function MovieModal({ isOpen, onOpenChange, selectedMovie }) {
   const [trailer, setTrailer] = useState(null);
+  const [showTrailer, setShowTrailer] = useState(null);
+
   useEffect(() => {
+    setShowTrailer(false);
     const fetchData = async () => {
       try {
         const trailerData = await fetchTrailer(selectedMovie.id);
@@ -24,10 +27,60 @@ function MovieModal({ isOpen, onOpenChange, selectedMovie }) {
 
     fetchData();
   }, [selectedMovie]);
+  const backDropImage = `https://image.tmdb.org/t/p/original${selectedMovie.backdrop_path}`;
 
+  return (
+    <Modal isOpen={isOpen} onOpenChange={onOpenChange} size="5xl">
+      <ModalContent>
+        {(onClose) => (
+          <div
+            className="p-8 min-h-[800px] text-white text-shadow relative bg-cover bg-center"
+            style={{
+              backgroundImage: `url(${backDropImage})`,
+            }}
+          >
+            {showTrailer ? (
+              <iframe
+                className="rounded-xl"
+                width="100%"
+                height="750"
+                src={`https://www.youtube.com/embed/${trailer.key}`}
+                title="Movie Trailer"
+                frameBorder="0"
+                allowFullScreen
+              />
+            ) : (
+              <MovieDetails
+                selectedMovie={selectedMovie}
+                trailer={trailer}
+                setShowTrailer={setShowTrailer}
+              />
+            )}
+            <ModalFooter>
+              <Button
+                color="danger"
+                variant="light"
+                onPress={() => {
+                  onClose();
+                  setShowTrailer(false);
+                }}
+              >
+                Close
+              </Button>
+              <Button color="primary" onPress={onClose}>
+                Action
+              </Button>
+            </ModalFooter>
+          </div>
+        )}
+      </ModalContent>
+    </Modal>
+  );
+}
+
+const MovieDetails = ({ selectedMovie, trailer, setShowTrailer }) => {
   const { watchlist, addMovieToWatchlist, removeMovieFromWatchlist } =
     useWatchlistContext();
-  const backDropImage = `https://image.tmdb.org/t/p/original${selectedMovie.backdrop_path}`;
 
   const handleAddToWatchlist = (movie) => {
     addMovieToWatchlist(movie);
@@ -50,82 +103,56 @@ function MovieModal({ isOpen, onOpenChange, selectedMovie }) {
 
   // Format the date string as desired
   const formattedRealeseDate = `${month}/${day}/${year}`; // Example format: YYYY-MM-DD
-
   return (
-    <Modal isOpen={isOpen} onOpenChange={onOpenChange} size="5xl">
-      <ModalContent>
-        {(onClose) => (
-          <div
-            className="p-8 min-h-[800px] text-white text-shadow relative bg-cover bg-center"
-            style={{
-              backgroundImage: `url(${backDropImage})`,
-            }}
-          >
-            <ModalHeader className="text-4xl">
-              {selectedMovie.title ? selectedMovie.title : selectedMovie.name}
-              <span className="px-2 font-normal text-slate-300">({year})</span>
-            </ModalHeader>
-            <ModalBody className="md:w-1/2 p-8 rounded-lg text-shadow-sm bg-black/40 shadow-lg">
-              <div className="flex gap-5">
-                <p>R</p>
-                <p>1h50m</p>
-                <p>{formattedRealeseDate}</p>
-              </div>
-              <p>{selectedMovie.overview}</p>
-              <div className="flex gap-5">
-                <p>Horror</p>
-                <p>Occult</p>
-              </div>
-              <div>
-                {isMovieInWatchlist() ? (
-                  <Button
-                    color="primary"
-                    onClick={() => handleRemoveFromWatchlist(selectedMovie)}
-                  >
-                    - Watchlist
-                  </Button>
-                ) : (
-                  <Button
-                    color="primary"
-                    onClick={() => handleAddToWatchlist(selectedMovie)}
-                  >
-                    + Watchlist
-                  </Button>
-                )}
-                {trailer && (
-                  <a
-                    className="mx-4 p-3"
-                    href={`https://www.youtube.com/watch?v=${trailer.key}`}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    Trailer
-                  </a>
-                )}
-
-                {/* <iframe
-                  width="100%"
-                  height="400"
-                  src={`https://www.youtube.com/embed/${trailer.key}`}
-                  title="Movie Trailer"
-                  frameBorder="0"
-                  allowFullScreen
-                /> */}
-              </div>
-            </ModalBody>
-            <ModalFooter>
-              <Button color="danger" variant="light" onPress={onClose}>
-                Close
-              </Button>
-              <Button color="primary" onPress={onClose}>
-                Action
-              </Button>
-            </ModalFooter>
-          </div>
-        )}
-      </ModalContent>
-    </Modal>
+    <>
+      <ModalHeader className="text-4xl">
+        {selectedMovie.title ? selectedMovie.title : selectedMovie.name}
+        <span className="px-2 font-normal text-slate-300">({year})</span>
+      </ModalHeader>
+      <ModalBody className="md:w-1/2 p-8 rounded-lg text-shadow-sm bg-black/40 shadow-lg">
+        <div className="flex gap-5">
+          <p>R</p>
+          <p>1h50m</p>
+          <p>{formattedRealeseDate}</p>
+        </div>
+        <p>{selectedMovie.overview}</p>
+        <div className="flex gap-5">
+          <p>Horror</p>
+          <p>Occult</p>
+        </div>
+        <div>
+          {isMovieInWatchlist() ? (
+            <Button
+              color="primary"
+              onClick={() => handleRemoveFromWatchlist(selectedMovie)}
+            >
+              - Watchlist
+            </Button>
+          ) : (
+            <Button
+              color="primary"
+              onClick={() => handleAddToWatchlist(selectedMovie)}
+            >
+              + Watchlist
+            </Button>
+          )}
+          {trailer && (
+            //   <a
+            //     className="mx-4 p-3"
+            //     href={`https://www.youtube.com/watch?v=${trailer.key}`}
+            //     target="_blank"
+            //     rel="noopener noreferrer"
+            //   >
+            //     Trailer
+            //   </a>
+            <Button color="primary" onClick={() => setShowTrailer(true)}>
+              Play Trailor
+            </Button>
+          )}
+        </div>
+      </ModalBody>
+    </>
   );
-}
+};
 
 export default MovieModal;
